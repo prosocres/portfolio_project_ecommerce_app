45be8a54ded3e8872de364b10c86ab380d57fdb5
Danny Schuster <dannyschu@Dannys-MacBook-Air-Office.local>
2024-07-24 09:54:31 -0400

    comments on the res.status from the signupUser function, successfully adds customer, except user_role

--- db/auth-controller.js
+++ db/auth-controller.js
@@ -28,7 +28,7 @@ const signupUser = async (req, res, next) => {
 
       const newUser = await createUser(user)
       //const newCart = await createCart(newUser.id)
-      res.status(201).json({userId: newUser.id})
+      //res.status(201).json({userId: newUser.id})
         // cartId: newCart.id
       next()
 }
